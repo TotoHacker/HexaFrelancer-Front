33fb4445519950088b0c0737f32a39d64a6237a4
Josué138 <c.oviedo@dappertechnologies.com>
2024-11-25 18:20:32 -0500

    Update dashboard

--- src/app/main/modules/dashboard/pages/dashboard-main-page/dashboard-main-page.component.ts
+++ src/app/main/modules/dashboard/pages/dashboard-main-page/dashboard-main-page.component.ts
@@ -12,6 +12,7 @@ import { ButtonModule } from 'primeng/button';
 import { BlogEntry } from '../../../blog/models/blog-models.model';
 import { PrimeNgModule } from 'src/app/primeng.module';
 import { ChartModule } from 'primeng/chart';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
     standalone: true,
@@ -26,6 +27,7 @@ import { ChartModule } from 'primeng/chart';
         ButtonModule,
         PrimeNgModule,
         ChartModule,
+        CarouselModule,
     ],
     selector: 'app-dashboard-main-page',
     templateUrl: './dashboard-main-page.component.html',
@@ -37,91 +39,95 @@ export class DashboardMainPageComponent implements OnInit {
         photoURL: 'https://firebasestorage.googleapis.com/v0/b/cocity-loft.appspot.com/o/clients%2F98a6u3xynd7.jpeg?alt=media&token=73c217a9-6805-4465-bbae-121873353d60',
     };
 
-
-    constructor(
-    ) {
-    }
-    data: any;
-    options: any;
+    constructor() { }
     data2: any;
     options2: any;
+
+    products: any[] = [
+        {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+        {
+            id: '1000',
+            code: 'f230fh0g3',
+            name: 'Bamboo Watch',
+            description: 'Product Description',
+            image: 'bamboo-watch.jpg',
+            price: 65,
+            category: 'Accessories',
+            quantity: 24,
+            inventoryStatus: 'INSTOCK',
+            rating: 5
+        },
+    ];
+
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1199px',
+            numVisible: 1,
+            numScroll: 1
+        },
+        {
+            breakpoint: '991px',
+            numVisible: 2,
+            numScroll: 1
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];;
 
     ngOnInit() {
         const documentStyle = getComputedStyle(document.documentElement);
         const textColor = documentStyle.getPropertyValue('--text-color');
         const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
         const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-        this.data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        this.data2 = {
+            labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
             datasets: [
                 {
-                    type: 'line',
-                    label: 'Dataset 1',
-                    borderColor: documentStyle.getPropertyValue('--blue-500'),
-                    borderWidth: 2,
-                    fill: false,
-                    tension: 0.4,
-                    data: [50, 25, 12, 48, 56, 76, 42]
-                },
-                {
-                    type: 'bar',
-                    label: 'Dataset 2',
-                    backgroundColor: documentStyle.getPropertyValue('--green-500'),
-                    data: [21, 84, 24, 75, 37, 65, 34],
-                    borderColor: 'white',
-                    borderWidth: 2
-                },
-                {
-                    type: 'bar',
-                    label: 'Dataset 3',
-                    backgroundColor: documentStyle.getPropertyValue('--orange-500'),
-                    data: [41, 52, 24, 74, 23, 21, 32]
-                }
-            ]
-        };
-
-        this.options = {
-            maintainAspectRatio: false,
-            aspectRatio: 0.6,
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder
-                    }
-                }
-            }
-        };
-
-       this.data2 = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'My First dataset',
+                    label: 'Mensajes',
                     backgroundColor: documentStyle.getPropertyValue('--blue-500'),
                     borderColor: documentStyle.getPropertyValue('--blue-500'),
                     data: [65, 59, 80, 81, 56, 55, 40]
                 },
                 {
-                    label: 'My Second dataset',
+                    label: 'Completados',
                     backgroundColor: documentStyle.getPropertyValue('--pink-500'),
                     borderColor: documentStyle.getPropertyValue('--pink-500'),
                     data: [28, 48, 40, 19, 86, 27, 90]
